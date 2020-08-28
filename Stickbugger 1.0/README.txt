410654eb10c11f4e2e518916ce20f77cb6c54083
FreyDragon <70413900+FreyDragon@users.noreply.github.com>
2020-08-28 17:19:34 -0500

    Add files via upload

---
+++ Stickbugger 1.0/README.txt
@@ -0,0 +1,8 @@
+THIS IS FOR FUN.
+
+IF YOU COPY THIS PLEASE GIVE CREDIT AND LINK MY GITHUB.
+
+USE:
+
+Take the file: SPECIAL_GIFT and give it to your friend
+Laugh when they open it, and share it with more people.
---
+++ Stickbugger 1.0/SPECIAL_GIFT.txt.cmd
@@ -0,0 +1,28 @@
+start https://www.youtube.com/watch?v=fC7oUOUEEi4
+
+cd C:\
+
+mkdir lol
+
+cd C:\lol
+
+echo "Get stick bugged lol, this is harmless, spread this to your friends. Github: https://github.com/FreyDragon/Sitckbugger/projects . " > "StickBug.txt"
+
+timeout 9
+
+.\StickBug.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
